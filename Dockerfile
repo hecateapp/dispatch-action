f7d7b0679ba3f18d611db298475cc568c3c2bcc8
John Barton (joho) <jrbarton@gmail.com>
2019-02-15 21:18:25 +1100

    start sketching out the go side

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 # Build stage
-FROM golang:alpine AS build-stage
+FROM golang:1.12rc1-alpine3.9 AS build-stage
 
 ADD . /src
+RUN go get gopkg.in/go-playground/webhooks.v5/github
 RUN cd /src && go build -o dispatch
 
 # Release stage
-FROM alpine
-WORKDIR /dispatch
-COPY --from=build-stage /src/dispatch /dispatch/
+FROM alpine:3.9
+COPY --from=build-stage /src/dispatch /
 
 LABEL version="1.0.0"
 LABEL repository="https://github.com/hecateapp/dispatch-action"
@@ -19,4 +19,4 @@ LABEL "com.github.actions.description"="Sends merge emails to whoever needs them
 LABEL "com.github.actions.icon"="at-sign"
 LABEL "com.github.actions.color"="purple"
 
-ENTRYPOINT ["./dispatch"]
+ENTRYPOINT ["/dispatch"]
--- dispatch.go
+++ dispatch.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"gopkg.in/go-playground/webhooks.v5/github"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	githubEventPath := os.Getenv("GITHUB_EVENT_PATH")
+	// githubToken := os.Getenv("GITHUB_TOKEN")
+	eventFile, err := ioutil.ReadFile(githubEventPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var event github.PullRequestPayload
+	err = json.Unmarshal(eventFile, &event)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if event.Action == "closed" && event.PullRequest.Merged {
+		// call hecate api asking for an email
+		log.Printf("Merged PR #%v.", event.PullRequest.Number)
+	} else {
+		log.Printf("No need to do anything for PR #%v. Action: %v, merged: %v", event.PullRequest.Number, event.Action, event.PullRequest.Merged)
+		os.Exit(78)
+	}
 }
